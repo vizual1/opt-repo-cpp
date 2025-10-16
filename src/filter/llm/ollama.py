d5e8d0357d0b94b03cd0c118c2674e1acbbc1148
Tommy Ho <tho@student.ethz.ch>
2025-10-16 16:15:50 +0200

    prompt logging for ollama

--- src/filter/llm/ollama.py
+++ src/filter/llm/ollama.py
@@ -1,8 +1,7 @@
 import logging
 from src.filter.llm.llmadapter import LLMAdapter
 from src.filter.llm.prompt import Prompt
-import requests, os
-
+import requests
 
 class OllamaLLM(LLMAdapter):
     def __init__(self, model: str):
@@ -10,6 +9,7 @@ class OllamaLLM(LLMAdapter):
 
     def _send_request(self, prompt: Prompt) -> str:
         full_prompt = "\n".join([m.content for m in prompt.messages]).strip()
+        logging.info(f"LLM prompt: {full_prompt}")
         response = requests.post(f"http://localhost:11434/api/generate", json={
             "model": self.model,
             "prompt": full_prompt,
